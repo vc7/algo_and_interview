2102778bda15edf6e02ba526f2e4b4f8bb1f8fdd
Lihsuan Chen <vince78718@gmail.com>
2020-03-17 01:27:41 +0900

    Update minimumSteps.swift

--- companies/mercari/minimumSteps.swift
+++ companies/mercari/minimumSteps.swift
@@ -1,1 +1,11 @@
-
+func minimumSteps(loggedMoves: [String]) -> Int {
+  var result = 0
+  for move in loggedMoves {
+    switch move {
+      case "../": result -= 1
+      case "./": break
+      default: result += 1
+    }
+  }
+  return result
+}
